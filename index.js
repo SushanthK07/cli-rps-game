0d3cff98a96a772c82d62e873efdc2a6a810b056
SushanthK07 <sushanthkummitha@gmail.com>
2024-03-03 18:20:52 +0530

    inquirer prompts for name and age

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 #!/usr/bin/env node
 
 import chalk from "chalk";
+import inquirer from "inquirer";
 
-console.log(chalk.bgGreen("Hi"));
+const answers = await inquirer.prompt([
+  {
+    type: "input",
+    name: "username",
+    message: "Enter your username",
+  },
+  {
+    type: "number",
+    name: "age",
+    message: "Enter your age",
+  },
+]);
+
+console.log(chalk.bgGreen(`Hi ${answers.username}`));
+console.log(chalk.bold(`You're ${answers.age} years old`));
